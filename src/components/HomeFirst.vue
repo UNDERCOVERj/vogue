a867a7ac796fc8c53987e42ba98286216b943ba4
UNDERCOVERj <766348980@qq.com>
2017-02-21 21:10:54 +0800

    improve slider

--- src/components/HomeFirst.vue
+++ src/components/HomeFirst.vue
@@ -4,18 +4,26 @@
 			<h1 :style="{height:home_first_height_margin+'px',lineHeight:home_first_height_margin+'px'}">nigo&kaws</h1>
 			<router-link to="/collection" tag="img" src="../static/images/kaws.jpg" alt="kaws" class="kaws"></router-link>
 		</div>
-		<div class="wrapper" :style="{height:home_first_height+'px'}">
-			<ul :class="['shuffling-bar',{trans : open, transnext: openNext, transpre: openPre}]" @mouseover="clear" @mouseout="autoScroll" :style="{top:home_first_height_margin-20+'px'}">
-				<li v-for="index in 4" :class="['item', {pre: index == preIndex ,next: index == nextIndex ,current: index == shufflingIndex}]" >
-					<img :src="'../static/images/home_'+index+'.jpg'"/>
-				</li>
-				<div @click="click_next" class="btn left_btn"></div>
-				<div @click="click_pre" class="btn right_btn"></div>
-				<ul class="round_ul">
-					<li v-for="j in 4" class="round_li" :class="{active_round_li:j==shufflingIndex}" @click="round(j)"></li>
-				</ul>
-			</ul>
-			
+		<div class="container" :style="{height:home_first_height+'px'}">
+			<div class="wrapper" :style="{width:originalData.img_width+'px',height:originalData.img_height+'px',top:home_first_height_margin-20+'px'}" @mouseover="stop" @mouseout="play">
+	    	<div class="wrapper-content" :class="{wrapper_trans:isTrans}" :style="{width:originalData.img_width*(originalData.num+2)+'px',height:originalData.img_height+'px',left:-originalData.img_width+'px'}" ref="wrapperContent">
+	    		<img class="wrapper-content_img" alt="4" :src="'../static/images/home_4.jpg'" :style="{width:originalData.img_width+'px',height:originalData.img_height+'px'}"/>
+	    		<img class="wrapper-content_img" alt="1" :src="'../static/images/home_1.jpg'" :style="{width:originalData.img_width+'px',height:originalData.img_height+'px'}"/>
+	    		<img class="wrapper-content_img" alt="2" :src="'../static/images/home_2.jpg'" :style="{width:originalData.img_width+'px',height:originalData.img_height+'px'}"/>
+	    		<img class="wrapper-content_img" alt="3" :src="'../static/images/home_3.jpg'" :style="{width:originalData.img_width+'px',height:originalData.img_height+'px'}"/>
+	    		<img class="wrapper-content_img" alt="4" :src="'../static/images/home_4.jpg'" :style="{width:originalData.img_width+'px',height:originalData.img_height+'px'}"/>
+	    		<img class="wrapper-content_img" alt="1" :src="'../static/images/home_1.jpg'" :style="{width:originalData.img_width+'px',height:originalData.img_height+'px'}"/>
+	    	</div>
+	    	<div class="wrapper-buttons" :style="{left:(originalData.img_width-100)/2+'px'}">
+	    		<span :class="['wrapper-button',{'wrapper_on':index==1}]"  @click="turnTo(1)"></span>
+	    		<span :class="['wrapper-button',{'wrapper_on':index==2}]"  @click="turnTo(2)"></span>
+	    		<span :class="['wrapper-button',{'wrapper_on':index==3}]"  @click="turnTo(3)"></span>
+	    		<span :class="['wrapper-button',{'wrapper_on':index==4}]"  @click="turnTo(4)"></span>
+	    	</div>
+
+	    	<a href="javascript:;" class="wrapper-arrow wrapper-prev" :style="{marginTop:-originalData.btn_width/2+'px'}" @click="prev"></a>
+	    	<a href="javascript:;" class="wrapper-arrow wrapper-next" :style="{marginTop:-originalData.btn_width/2+'px'}" @click="next"></a>
+    	</div>
 		</div>
 	</div>
 </template>
@@ -24,14 +32,18 @@ import {mapGetters} from 'vuex'
 	export default {
 		data (){
 			return {
-				shufflingIndex: 1, //当前元素的index
-		        nextIndex: 0, //后一个元素的序列
-		        preIndex: 0, //前一个元素的序列
-		        open:true,
-		        openPre:true,
-		        openNext:false,
-		        interval_timer:null,
-		        timeout_timer:null
+				originalData:{
+    				img_width:350,
+    				img_height:350,
+    				btn_width:40,
+    				btn_height:40,
+    				num:4,
+    				delay:300
+    			},
+    			isTrans:true,//因为到最后一张图片，index为1时，需要立即跳到第二张index也为1的图片，这个用来是否给出transition
+    			index:1,
+    			timer:null,//setInterval
+    			clickdelay:false//用来防止连续点击
 			}
 		},
 		computed:{
@@ -51,103 +63,100 @@ import {mapGetters} from 'vuex'
 			}
 		},
 		methods:{
-			
-		      caculateIndex(){ //计算上一个元素和下一个元素的index
-		        this.preIndex = this.shufflingIndex - 1 < 1 ? 4: this.shufflingIndex - 1;
-		        this.nextIndex = this.shufflingIndex + 1 > 4 ? 1 : this.shufflingIndex + 1;
-		      },
-		      autoScroll(){
-		      	var self=this
-		      	this.clear()
-		      	this.openNext=false;
-			     this.openPre=true;
-		      	this.interval_timer=setInterval(self.autoChange,2000)
-		      },
-		      autoChange(){
-		      	if(this.shufflingIndex == 4){
-		          	this.shufflingIndex = 1;
-		        }else{
-		           	this.shufflingIndex++;
-		        }	
-		        this.caculateIndex();
-		      },
-		      //向前面移动
-		      click_pre(){
-		      	
-		      	if(this.timeout_timer){
-		      		clearTimeout(this.timeout_timer)
-		      	}
-		      	var self=this;
-		      	self.openNext=false;
-			     	self.openPre=true;
-		      	this.timeout_timer=setTimeout(function(){
-		      		
-			     	self.autoChange();
-		      	},300)	
-		      },
-		      //向后面移动
-		      click_next(){
-		      	
-		      	if(this.timeout_timer){
-		      		clearTimeout(this.timeout_timer)
-		      	}
-		      	var self=this;
-		      	self.openNext=true;
-			     	self.openPre=false;
-		      	
-		      	this.timeout_timer=setTimeout(function(){
-			     	self.shufflingIndex=self.shufflingIndex==1?4:self.shufflingIndex-1
-			     	self.caculateIndex()
-		      	},300)
-		      	
-		      },
-		      clear(){
-		      	if(this.interval_timer){
-		      		clearInterval(this.interval_timer)
-		      	}
-		      },
-		      round(index){
-		      	var self=this
-		      	//点击圆点，就相当于点多少次左右键
-		        if(this.shufflingIndex<index){
-			      	this.openNext=false;
-			      	this.openPre=true;
-			      		this.interval_timer=setInterval(function(){
-			      			if(self.shufflingIndex==index){
-			      				self.clear()		
-			      			}else{
-			      				self.shufflingIndex++;
-			      				self.caculateIndex();
-			      			}
-			      			//必须要用if，else来clear
-			      		},200)
-			      	      	
-		      		
-		      	}else if(this.shufflingIndex>index){
-		      		
-		      		this.openNext=true;
-			      	this.openPre=false;
-		      		
-			      		this.interval_timer=setInterval(function(){
-			      			if(self.shufflingIndex==index){
-			      				self.clear()
-			      			}else{
-			      				self.shufflingIndex--;
-			      				self.caculateIndex();
-			      			}
-			      			//必须要用if，else来clear
-			      		},200)
-			      	 
-			      	 	
-			      	
-		      	}
-		      	
-		      }
-		},
+    			next(){
+    				if(this.clickdelay){
+    					return 
+    				}
+    				this.clickdelay=true
+    				if(this.index==this.originalData.num){
+    					this.index=1
+    				}else{
+    					
+    					this.index+=1
+    				}
+    				this.animate(this.originalData.img_width)
+    				
+    			},
+    			prev(){
+    				if(this.clickdelay){
+    					return 
+    				}
+    				this.clickdelay=true
+    				if(this.index==1){
+    					this.index=this.originalData.num
+    				}else{
+    					this.index-=1
+    				}
+    				this.animate(-this.originalData.img_width)	
+    			},
+    			animate(offset){
+    				var node=this.$refs.wrapperContent
+    				var self=this;
+    				var left=parseInt(node.style.left)-offset
+    				this.isTrans=true
+    				node.style.left=left+'px'
+    				setTimeout(function(){
+    					if(left<-(self.originalData.num*self.originalData.img_width)){
+    						self.isTrans=false
+    						node.style.left=-self.originalData.img_width+'px'
+    						self.clickdelay=false //当到达最后一张图片时 
+    					}
+    					if(left>-100){
+    						self.isTrans=false
+    						node.style.left=-self.originalData.num*self.originalData.img_width+'px'
+    						self.clickdelay=false //当到达第一张图片时  
+    					}
+    				},this.originalData.delay)
+    			},
+    			play(){
+    			
+    				var self=this;
+    				this.timer=setInterval(function(){
+    					self.next()
+    				},2000)
+    			},
+    			stop(){
+    				this.clickdelay=false//用来防止连续点击
+    				clearInterval(this.timer)
+    				this.timer=null
+    			},
+    			turnTo(flag){
+    				if(flag==this.index){
+    					return
+    				}else{
+    					var offset=(flag-this.index)*this.originalData.img_width
+    					this.index=flag
+    					this.animate(offset)
+
+    				}
+
+    			}
+    		},
+    		
+    		mounted(){
+    			/*下面是判断过渡动画是否完成*/ 
+    			var node=this.$refs.wrapperContent
+    			var transitions = {
+			         'transition':'transitionend',
+			         'OTransition':'oTransitionEnd',
+			         'MozTransition':'transitionend',
+			         'WebkitTransition':'webkitTransitionEnd'
+     				}
+     				var self=this
+ 
+		       for(var t in transitions){
+
+		           if( node.style[t] !== undefined ){
+		               var transitionEvent=transitions[t];
+		           }
+		       }
+		       transitionEvent && node.addEventListener(transitionEvent, function() {
+   					self.clickdelay=false              
+   			   });
+    		   this.play()
+    		},
 		created(){
 			this.$store.dispatch('changeShow','home')
-			this.caculateIndex();
-			this.autoScroll();
 		}
 	}
 </script>
@@ -158,45 +167,18 @@ import {mapGetters} from 'vuex'
 	.home_first_left h1{text-align: center;font-family: TrashHand;font-size: 32px;
 color:darkslateblue;}
 	.kaws{border: none;height: 300px;height: 300px}
-	.wrapper{margin-left: 400px;}
-	.shuffling-bar{position: relative; overflow: hidden;margin: auto}
-	.item{ position: absolute;top: 0;bottom: 0;left: 0;right: 0;z-index:100;}
-	 .current{
-      	transform: translate3d(0, 0, 0);
-		-ms-transform: translate3d(0, 0, 0);		/* IE 9 */
-		-webkit-transform: translate3d(0, 0, 0);	/* Safari and Chrome */
-		-o-transform: translate3d(0, 0, 0);		/* Opera */
-		-moz-transform: translate3d(0, 0, 0);
-     	z-index: 300;
-    }
-    .pre{
-	  transform: translate3d(-350px, 0, 0);
-	  -ms-transform: translate3d(-350px, 0, 0);		/* IE 9 */
-	  -webkit-transform: translate3d(-350px, 0, 0);	/* Safari and Chrome */
-	  -o-transform: translate3d(-350px, 0, 0);		/* Opera */
-	  -moz-transform: translate3d(-350px, 0, 0);
-      z-index: 300;
-    }
-    .next{
-	  transform: translate3d(350px, 0, 0);
-	  -ms-transform: translate3d(350px, 0, 0);		/* IE 9 */
-	  -webkit-transform: translate3d(350px, 0, 0);	/* Safari and Chrome */
-	  -o-transform: translate3d(350px, 0, 0);		/* Opera */
-	  -moz-transform: translate3d(350px, 0, 0);
-      z-index: 300;
-    }
-   .trans .current,.transpre .pre,.transnext .next{
-        transition: transform .2s ease-in-out;
-		-moz-transition: transform .2s ease-in-out;	/* Firefox 4 */
-		-webkit-transition: transform .2s ease-in-out;	/* Safari 和 Chrome */
-		-o-transition: transform .2s ease-in-out;	/* Opera */ 
-  }
-  .shuffling-bar img,.shuffling-bar,.item{width: 350px;height: 350px;}
-  .btn{position: absolute;z-index:500;width: 50px;height: 200px;top:75px;cursor: pointer;opacity: 0.5}
-  .left_btn{left:0;}
-  .right_btn{right:0;}
-  .btn:hover{opacity: 1}
-  .round_ul{text-align: center;position: absolute;z-index: 500;width: 100%;bottom: 10px;}
-  .round_li{display: inline-block;width: 20px;height: 20px;border-radius: 10px;background: green;z-index:400;margin:0 5px;cursor:pointer;}
-  .active_round_li{background: red}
+	.container{margin-left: 400px;}
+	
+	 .wrapper{position: relative;overflow: hidden;margin: auto}
+    .wrapper-content{position: absolute;left: 0;z-index: 1;}
+    .wrapper-content_img{border: none;outline:none;float: left}
+    .wrapper-buttons{position: absolute;width: 100px;height: 20px;text-align: center;bottom: 3px;z-index: 2;}
+    .wrapper-button{float: left;width: 20px;height: 20px;border-radius: 10px;background: lightblue;margin: 0 2.5px;cursor: pointer;}
+   .wrapper-arrow{position: absolute;width: 40px;height:40px;cursor: pointer;display: none;top:50%;z-index: 2; }
+  
+    .wrapper:hover .wrapper-arrow{display: block;opacity:1}
+    .wrapper-prev{left:10px;}
+    .wrapper-next{right:10px;}
+    .wrapper_on{background-color: #00BC9B}
+    .wrapper_trans{transition: left .3s ease}
 </style>
